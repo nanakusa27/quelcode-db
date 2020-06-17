1bfb92da441f1bdd7b4d3fc257073496eeb9d05f
nanakusa27 <masakiw27@yahoo.co.jp>
2020-06-17 17:08:57 +0900

    Fix:chatroomsテーブルのカラム名変更、外部キー書き方修正

--- db/db-challenge2/chatwork.sql
+++ db/db-challenge2/chatwork.sql
@@ -9,33 +9,35 @@ CREATE TABLE users(
     is_deleted TINYINT(1) NOT NULL DEFAULT 0,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
-    primary key(user_id)
+    PRIMARY KEY(user_id)
 );
 
 CREATE TABLE chat_members(
-    chatroom_id INT(11) NOT NULL REFERENCES chatrooms(chatroom_id),
-    user_id INT(11) NOT NULL REFERENCES users(users),
+    chatroom_id INT(11) NOT NULL,
+    user_id INT(11) NOT NULL,
     joined_at DATETIME NOT NULL,
-    primary key(chatroom_id, user_id)
+    PRIMARY KEY(chatroom_id, user_id),
+    FOREIGN KEY (chatroom_id) REFERENCES chatrooms(chatroom_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE chatrooms(
     chatroom_id INT(11) NOT NULL AUTO_INCREMENT,
     chatroom_name VARCHAR(100) NOT NULL,
     about VARCHAR(1000),
-    is_can_send TINYINT(1) NOT NULL DEFAULT 1,
+    is_send_able TINYINT(1) NOT NULL DEFAULT 1,
     is_direct_chat TINYINT(1) NOT NULL DEFAULT 0,
     is_deleted TINYINT(1) NOT NULL DEFAULT 0,
     created_at DATETIME,
     created_by_user_id INT(11) NOT NULL,
     updated_at DATETIME,
     updated_by_user_id INT(11) NOT NULL,
-    primary key(chatroom_id)
+    PRIMARY KEY(chatroom_id)
 );
 
 CREATE TABLE chats(
     chat_id INT(11) NOT NULL AUTO_INCREMENT,
-    chatroom_id INT(11) NOT NULL REFERENCES chatrooms(chatroom_id),
+    chatroom_id INT(11) NOT NULL,
     message VARCHAR(1000) NOT NULL,
     file_name VARCHAR(100),
     is_deleted TINYINT(1) NOT NULL DEFAULT 0,
@@ -43,12 +45,13 @@ CREATE TABLE chats(
     created_by_user_id INT(11) NOT NULL,
     updated_at DATETIME NOT NULL,
     updated_by_user_id INT(11) NOT NULL,
-    primary key(chat_id)
+    PRIMARY KEY(chat_id),
+    FOREIGN KEY (chatroom_id) REFERENCES chatrooms(chatroom_id)
 );
 
 CREATE TABLE tasks(
     task_id INT(11) NOT NULL AUTO_INCREMENT,
-    chatroom_id INT(11) NOT NULL REFERENCES chatrooms(chatroom_id),
+    chatroom_id INT(11) NOT NULL,
     assign_user_id INT(11) NOT NULL,
     task_content VARCHAR(1000) NOT NULL,
     deadline DATETIME NOT NULL,
@@ -58,5 +61,6 @@ CREATE TABLE tasks(
     created_by_user_id INT(11) NOT NULL,
     updated_at DATETIME NOT NULL,
     updated_by_user_id INT(11) NOT NULL,
-    primary key(task_id)
+    PRIMARY KEY(task_id),
+    FOREIGN KEY (chatroom_id) REFERENCES chatrooms(chatroom_id)
 );
