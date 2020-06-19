df37bdc69477267f09510f875d972103ff60801d
nanakusa27 <masakiw27@yahoo.co.jp>
2020-06-19 17:21:54 +0900

    Improve: db-007.sql case文の書き方変更

--- db/db-challenge3/db-007/db-007.sql
+++ db/db-challenge3/db-007/db-007.sql
@@ -1,4 +1,4 @@
-SELECT chatroom_id, chatroom_name, about, is_sendable, is_deleted, created_at, created_by_user_id, updated_at, updated_by_user_id, CASE WHEN 1 = is_sendable THEN '許可' ELSE '禁止' END
+SELECT chatroom_id, chatroom_name, about, is_sendable, is_deleted, created_at, created_by_user_id, updated_at, updated_by_user_id, CASE WHEN is_sendable = 1 THEN '許可' ELSE '禁止' END
 FROM chatrooms
 WHERE about like '%ダイレクトチャット'
 AND is_deleted = 0
