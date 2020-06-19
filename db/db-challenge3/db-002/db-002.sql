19510128dc546a940ce9d631331b7c09dfce55ef
nanakusa27 <masakiw27@yahoo.co.jp>
2020-06-19 12:02:08 +0900

    Fix: 課題提出方法の修正

---
+++ db/db-challenge3/db-002/db-002.sql
@@ -0,0 +1,11 @@
+INSERT INTO chatrooms (`chatroom_name`, `about`, `is_sendable`, `is_direct_chat`, `is_deleted`,`created_by_user_id`, `created_at`, `updated_by_user_id`, `updated_at`)
+VALUES
+('りんご', '利用者の集まり', DEFAULT, DEFAULT, DEFAULT, 1, '2020/04/21 11:26:32', 1, '2020/04/21 11:26:32'),
+('ごりら', 'sqlの集まり', DEFAULT, DEFAULT, DEFAULT, 2, '2020/04/23 15:42:05', 2, '2020/04/23 15:42:05'),
+('らっぱ', '言語の集まり', DEFAULT, DEFAULT, DEFAULT, 8, '2020/04/24 11:03:16', 9, '2020/04/25 10:33:55'),
+('ぱんだ', 'フレームワーク', 0, DEFAULT, DEFAULT, 5, '2020/04/23 18:14:28', 7, '2020/04/24 20:33:06'),
+('ごま', 'adminとmysqlのダイレクトチャット', DEFAULT, 1, DEFAULT, 1, '2020/04/23 14:34:58', 1, '2020/04/23 14:34:58'),
+('まり', 'adminとsqlserverのダイレクトチャット', 0, 1, DEFAULT, 1, '2020/04/23 14:45:22', 1, '2020/04/23 14:45:22'),
+('りす', 'adminとjsのダイレクトチャット', DEFAULT, 1, DEFAULT, 1, '2020/04/24 10:55:21', 1, '2020/04/24 10:55:21'),
+('かめ', 'adminとamazonのダイレクトチャット', DEFAULT, 1, 1, 1, '2020/04/24 11:02:20', 1, '2020/04/27 15:41:33')
+;
